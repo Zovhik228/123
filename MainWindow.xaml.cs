43780f2fd447bf08e0f10650b147771f94809dec
gobdanov <gobdanov@inbox.ru>
2026-02-27 11:37:14 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UP02.Pages.Main;
+
+namespace UP02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Frame mainFrame = new Frame();
+        public MainWindow()
+        {
+            mainFrame = this.MainFrame;
+            OpenPage(new PageAuthorization());
+            InitializeComponent();
+        }
+        public static void ClearFrame()
+        {
+            mainFrame.Content = null;
+        }
+
+        public static void OpenPage(Page page)
+        {
+            if (mainFrame != null)
+            {
+                mainFrame.Navigate(page);
+            }
+        }
+
+        public static void GoBack()
+        {
+            if (mainFrame != null && mainFrame.CanGoBack)
+            {
+                mainFrame.GoBack();
+            }
+        }
+    }
+}
